86a4025cc9aca92f5d09c8ad5043f02652e42251
Nick Henderson <nwh@stanford.edu>
2014-08-27 11:25:37 -0700

    WIP: organizing cublas types and module
    modified:   src/CUBLAS.jl
    new file:   src/libcublas_types.jl

--- src/CUBLAS.jl
+++ src/CUBLAS.jl
@@ -1,5 +1,62 @@
+# CUBLAS.jl
+#
+# Julia interface to CUBLAS.  Inspired by CUDArt.jl and using Clang.jl.
+#
+# Author: Nick Henderson <nwh@stanford.edu>
+# Created: 2014-08-27
+# License: MIT
+#
+
 module CUBLAS
 
 # package code goes here
+
+include("libcublas_types.jl")
+
+# get cublas status message
+function statusmessage(status)
+    if status == CUBLAS_STATUS_SUCCESS
+        return "cublas success"
+    end
+    if status == CUBLAS_STATUS_NOT_INITIALIZED
+        return "cublas not initialized"
+    end
+    if status == CUBLAS_STATUS_ALLOC_FAILED
+        return "cublas alloc failed"
+    end
+    if status == CUBLAS_STATUS_INVALID_VALUE
+        return "cublas invalid value"
+    end
+    if status == CUBLAS_STATUS_ARCH_MISMATCH
+        return "cublas arch mismatch"
+    end
+    if status == CUBLAS_STATUS_MAPPING_ERROR
+        return "cublas mapping error"
+    end
+    if status == CUBLAS_STATUS_EXECUTION_FAILED
+        return "cublas execution failed"
+    end
+    if status == CUBLAS_STATUS_INTERNAL_ERROR
+        return "cublas internal error"
+    end
+    if status == CUBLAS_STATUS_NOT_SUPPORTED
+        return "cublas not supported"
+    end
+    if status == CUBLAS_STATUS_LICENSE_ERROR
+        return "cublas license error"
+    end
+end
+
+# error handling function
+function statuscheck(status)
+    if status == CUBLAS_STATUS_SUCCESS
+        return nothing
+    end
+    # Because try/finally may disguise the source of the problem,
+    # let's show a backtrace here
+    warn("CUBLAS error triggered from:")
+    Base.show_backtrace(STDOUT, backtrace())
+    throw(statusmessage(status)
+end
 
 end # module
---
+++ src/libcublas_types.jl
@@ -0,0 +1,56 @@
+# libcublas_types.jl
+#
+# Initially generated with wrap_c from Clang.jl. Modified to remove anonymous
+# enums and add cublasContext.
+#
+# Author: Nick Henderson <nwh@stanford.edu>
+# Created: 2014-08-27
+# License: MIT
+#
+
+# begin enum cublasStatus_t
+typealias cublasStatus_t Uint32
+const CUBLAS_STATUS_SUCCESS = 0
+const CUBLAS_STATUS_NOT_INITIALIZED = 1
+const CUBLAS_STATUS_ALLOC_FAILED = 3
+const CUBLAS_STATUS_INVALID_VALUE = 7
+const CUBLAS_STATUS_ARCH_MISMATCH = 8
+const CUBLAS_STATUS_MAPPING_ERROR = 11
+const CUBLAS_STATUS_EXECUTION_FAILED = 13
+const CUBLAS_STATUS_INTERNAL_ERROR = 14
+const CUBLAS_STATUS_NOT_SUPPORTED = 15
+const CUBLAS_STATUS_LICENSE_ERROR = 16
+# end enum cublasStatus_t
+# begin enum cublasFillMode_t
+typealias cublasFillMode_t Uint32
+const CUBLAS_FILL_MODE_LOWER = 0
+const CUBLAS_FILL_MODE_UPPER = 1
+# end enum cublasFillMode_t
+# begin enum cublasDiagType_t
+typealias cublasDiagType_t Uint32
+const CUBLAS_DIAG_NON_UNIT = 0
+const CUBLAS_DIAG_UNIT = 1
+# end enum cublasDiagType_t
+# begin enum cublasSideMode_t
+typealias cublasSideMode_t Uint32
+const CUBLAS_SIDE_LEFT = 0
+const CUBLAS_SIDE_RIGHT = 1
+# end enum cublasSideMode_t
+# begin enum cublasOperation_t
+typealias cublasOperation_t Uint32
+const CUBLAS_OP_N = 0
+const CUBLAS_OP_T = 1
+const CUBLAS_OP_C = 2
+# end enum cublasOperation_t
+# begin enum cublasPointerMode_t
+typealias cublasPointerMode_t Uint32
+const CUBLAS_POINTER_MODE_HOST = 0
+const CUBLAS_POINTER_MODE_DEVICE = 1
+# end enum cublasPointerMode_t
+# begin enum cublasAtomicsMode_t
+typealias cublasAtomicsMode_t Uint32
+const CUBLAS_ATOMICS_NOT_ALLOWED = 0
+const CUBLAS_ATOMICS_ALLOWED = 1
+# end enum cublasAtomicsMode_t
+typealias cublasContext Void
+typealias cublasHandle_t Ptr{cublasContext}

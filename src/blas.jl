7200935a9b3ee89202a614c29b034fe56160286c
Nick Henderson <nwh@stanford.edu>
2014-08-28 16:51:55 -0700

    WIP: added scal! - need to switch these blas.jl calls to direct ccalls

--- src/blas.jl
+++ src/blas.jl
@@ -6,7 +6,9 @@
 
 export
 # Level 1
-    blascopy!
+    blascopy!,
+    scal!,
+    scal
 
 function cublascall(s::Symbol)
     return symbol("cublas"*string(s)*"_v2")
@@ -26,3 +28,31 @@ for (fname, elty) in ((:Dcopy,:Float64),
         end
     end
 end
+
+## scal
+for (fname, elty) in ((:Dscal,:Float64),
+                      (:Sscal,:Float32),
+                      (:Zscal,:Complex128),
+                      (:Cscal,:Complex64))
+    @eval begin
+        # SUBROUTINE DSCAL(N,DA,DX,INCX)
+        function scal!(n::Integer, DA::$elty, DX::CudaArray{$elty}, incx::Integer)
+            $(cublascall(fname))(cublashandle[1],n,[DA],DX,incx)
+            DX
+        end
+    end
+end
+# TODO: implement copy{T}(x::CudaArray{T})
+#scal{T}(n::Integer, DA::T, DX::CudaArray{T}, incx::Integer) = scal!(n, DA, copy(DX), incx)
+# In case DX is complex, and DA is real, use dscal/sscal to save flops
+#for (fname, elty, celty) in ((:Sscal, :Float32, :Complex64),
+#                             (:Dscal, :Float64, :Complex128))
+#    @eval begin
+#        # SUBROUTINE DSCAL(N,DA,DX,INCX)
+#        function scal!(n::Integer, DA::$elty, DX::CudaArray{$celty}, incx::Integer)
+#            DY = reinterpret($elty,DX,(2*n,))
+#            $(cublascall(fname))(cublashandle[1],2*n,[DA],DY,incx)
+#            DX
+#        end
+#    end
+#end
--- test/runtests.jl
+++ test/runtests.jl
@@ -20,3 +20,19 @@ test_blascopy!(Float32[1:m])
 test_blascopy!(Float64[1:m])
 test_blascopy!(Float32[1:m]+im*Float32[1:m])
 test_blascopy!(Float64[1:m]+im*Float64[1:m])
+
+# test scal!
+function test_scal!{T}(alpha,A::Array{T})
+    @test ndims(A) == 1
+    n1 = length(A)
+    d_A = CudaArray(A)
+    scal!(n1,alpha,d_A,1)
+    A1 = to_host(d_A)
+    @test_approx_eq(alpha*A,A1)
+end
+test_scal!(2.0f0,Float32[1:m])
+test_scal!(2.0,Float64[1:m])
+test_scal!(1.0f0+im*1.0f0,Float32[1:m]+im*Float32[1:m])
+test_scal!(1.0+im*1.0,Float64[1:m]+im*Float64[1:m])
+#test_scal!(2.0f0,Float32[1:m]+im*Float32[1:m])
+#test_scal!(2.0,Float64[1:m]+im*Float64[1:m])

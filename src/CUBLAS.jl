023e48674290b6187788335a4aa71d481e6573a7
lucasb-eyer <lucasb.eyer.be@gmail.com>
2015-10-24 14:21:12 +0200

    Allow switching devices. Use `__init__`.

--- src/CUBLAS.jl
+++ src/CUBLAS.jl
@@ -79,12 +79,29 @@ end
 
 include("libcublas.jl")
 
-# setup cublas handle
-cublashandle = cublasHandle_t[0]
-cublasCreate_v2(cublashandle)
-# destroy cublas handle at julia exit
-atexit(()->cublasDestroy_v2(cublashandle[1]))
+# Global CUBLAS handle used for higher-level BLAS functions.
+const cublashandle = cublasHandle_t[C_NULL]
+
+function use_current_device()
+    if cublashandle[1] != C_NULL
+        cublasDestroy_v2(cublashandle[1])
+        cublashandle[1] = C_NULL
+    end
+
+    # setup cublas handle
+    cublasCreate_v2(cublashandle)
+end
 
 include("blas.jl")
+
+function __init__()
+    use_current_device()
+    atexit(()->
+        if cublashandle[1] != C_NULL
+            cublasDestroy_v2(cublashandle[1])
+            cublashandle[1] = C_NULL
+        end
+    )
+end
 
 end # module
